088704d99e1328c139b3fd860bbbbbb2ea163d71
KisleK <kisle.kuhn1@gmail.com>
2017-05-31 16:36:33 -0500

    added index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: "Bottle Opener Hand Fidget Spinner", price: 20, description: "A fidget spinner you can open you beer with!")
+
+Product.create(name: "Superhero Baby Onesies", price: 25, description: "Make your baby a superhero!")
+
+Product.create(name: "Paintball Bow", price: 860, description: "Time to go paintball hunting!")
+
+Product.create(name: "Spitball Blaster Gun", price: 50, description: "Making spitballs even easier!")
+
+Product.create(name: "Salt Shotgun", price: 60, description: "Great for those pesky demons!")
+
+Product.create(name: "3-In-1 Emergency Car Tool", price: 50, description: "Window breaker, seatbelt cutter, and a flashlight? What more do you need?")
+
+Product.create(name: "World’s Smallest Rubik’s Cube", price: 8, description: "Teeny tiny rubix cube")
+
+Product.create(name: "Creepy Coin Eating Bank", price: 18, description: "Save some money, creep some people out")
+
+Product.create(name: "Chewbacca Seatbelt Cover", price: 20, description: "Do I really need to describ e this to you?")
