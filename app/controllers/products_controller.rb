c78d97921d1ccf37a0db02c74301efcb1b15d66e
KisleK <kisle.kuhn1@gmail.com>
2017-06-01 15:02:39 -0500

    added background/nav bar/font

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,52 @@
 class ProductsController < ApplicationController
  def index
-    @products = Product.all
-    
+   @products = Product.all    
  end
 
+ def new
+   #doenst need logic
+ end
+
+ def create
+  product = Product.new(name: params[:name],
+                        price: params[:price],
+                        description: params[:description]
+                        )
+  product.save
+  flash[:success] = "Product Successfully Created"
+  redirect_to "/products/#{product.id}"
+ end
+
+ def show
+   product_id = params[:id]
+   @product = Product.find_by(id: product_id)
+  
+ end
+
+ def edit
+  @product = Product.find(params[:id]) ##same as def show just shorter
+ end
+
+ def update
+  product = Product.find(params[:id])
+  product.assign_attributes(
+                            name: params[:name],
+                            price: params[:price],
+                            description: params[:description]
+                            )
+  product.save
+  flash[:success] = "Product Updated"
+  redirect_to "/products/#{product.id}"
+ end
+
+ def destroy
+  product = Product.find(params[:id])
+  product.destroy
+
+  flash[:danger] = "Product Deleted"
+  redirect_to "/"
+
+   
+ end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   get '/products' => 'products#index'
   get '/' => 'products#index'
 
-  # get '/products/new' => 'products#new'
-  # post '/products' => 'products#create'
-
-  # get '/products/:id' => 'products#show'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
   
-  # get '/products/:id/edit' => 'products#edit'
-  # patch '/products/:id' => 'products#update'
+  get '/products/:id' => 'products#show'
+  
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
 
-  # delete '/products/:id' => 'products#destroy'
+  delete '/products/:id' => 'products#destroy'
 end
